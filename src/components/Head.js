78fd97fa3bcd5cda88103981e92a7e539cbcb7db
fabian <fabianrojas.sab@gmail.com>
2021-11-01 23:41:41 -0500

    Se inicia proyecto

---
+++ src/components/Head.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Head = props => (
+    <div className ="container-fluid">
+        <div className ="row">
+            <div className="col ">
+                <p className="text-left">Left aligned text on all viewport sizes.</p>
+            </div>
+            <div className="col ">
+                <p className="text-end">Right aligned text on all viewport sizes.</p>
+            </div>
+        </div>
+        
+    </div>
+)
---
+++ src/components/Tittle.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Tittle = props =>(
+    <h1></h1>
+)
